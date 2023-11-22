0c56040ab19b7907764218eb06a87782523bce9b
j2d6 <j2d6.Pro@gmail.com>
2023-11-22 10:36:29 +0300

    fix users setCookie a faire

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const { getUniqueUser, getAllUsers, updateUserMiddleware, deleteUserMiddleware } = require('../middlewares/userMiddleware');
+const { getUniqueUser, getAllUsers, updateUserMiddleware, deleteUserMiddleware, findSimilarUsersMiddleware } = require('../middlewares/userMiddleware');
 var router = express.Router();
 
 /* GET users listing. */
@@ -7,6 +7,7 @@ router.get('/get', getUniqueUser);
 router.get('/all', getAllUsers);
 router.post('/update', updateUserMiddleware) //ACCEPT QUERYSTRING user_id AND USER OBJECT MUST BE IN THE RQ'S BPDY
 router.get('/delete', deleteUserMiddleware) // QUERY STRING user_id ** REQUIRED **
+router.get('/find', findSimilarUsersMiddleware)
 
 
 module.exports = router;
